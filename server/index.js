9ac4043c206c1b99493d9d34b892b2ead282b3c3
Ahmed Mohamed Ehab Gaber Elshazly <ahmed.shazly15@gmail.com>
2019-02-09 15:22:04 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const students = [
     { id: "21-094123", name: "Mathew White", github_username: "matheww", email: "matheww@whatever.com" },
     { id: "15-10312", name: "Dom Sundle", github_username: "domss", email: "domss.whatever.com" },
     { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg" }
+    { id: "40-14610", name: "Ahmed Elshazly", github_username: "Elshazlyyy", email: "Ahmed.gaberelshazly@student.guc.edu.eg" }
 ];
 
 app.get('/', (request, response) => {
