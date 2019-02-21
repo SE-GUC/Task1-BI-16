0f5a5f003f13bb3fa165364d4619a8bb00eba343
omarkhaled2000 <47145556+omarkhaled2000@users.noreply.github.com>
2019-02-21 18:22:24 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const students = [
     { id: "28-09121", name: "Omar Sherif", github_username: "osheriff", email: "omarr@whatever.com" },
     { id: "21-094123", name: "Mathew White", github_username: "matheww", email: "matheww@whatever.com" },
     { id: "15-10312", name: "Dom Sundle", github_username: "domss", email: "domss.whatever.com" },
-    { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg" }
+    { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg" },
+     {id: "40-20093", name: "omar khaled", github_username: "omarkhaled2000", email: "xexceeder@gmail.com"}
 ];
 
 app.get('/', (request, response) => {
